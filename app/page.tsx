1a5cd3d1410f2cda11ee382c2e3fd1c72e48c563
Bright Esosa Ogiesoba <157101723+BAREO14@users.noreply.github.com>
2024-02-03 18:18:07 +0000

    Brain racking

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (<>
   <h1>I am <b>Bright Esosa Ogiesoba</b></h1>
   <p>I am a Programmer who loves Programming</p>
-  <p>I love drinking water and there is usually a bottle of H<sub>2</sub>O on my desk anytime
-    im coing.O<sup>2</sup> is also very compulsory in the life of a programmer
+  <p>I love drinking H<sub>2</sub>O and there is usually a bottle of H<sub>2</sub>O on my desk anytime
+    i'm coing.  [O<sup>2</sup> is also very compulsory in the life of any programmer]
   </p>
   <h2>Apart from <u>coding</u>, there are other things I
     enjoy doing and they include writing songs,
@@ -13,7 +13,7 @@ export default function Home() {
   </h2>
   
     <p>I'm tring to stay focused and acqquire this 
-      skill and do away with <s>distractions</s>.It's not going to be easy, but God dey.
+      skill and do away with <s>distractions</s>.It's not going to be <i>easy</i>, but God dey.
     </p>
     <p>One more thing, I <s>deleted</s> the app I created and started over from 
       comm. prompt before I could <b>Finally</b> get this done.
